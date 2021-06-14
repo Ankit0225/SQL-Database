9eb58a1f7428c6bef9126d2d7f9496e2c9944865
Ankit <ankitmaurya0225@gmail.com>
2021-06-14 21:38:18 +0530

    mysql with express & hbs

--- MYSQL with Express Server/server.js
+++ MYSQL with Express Server/server.js
@@ -1,11 +1,38 @@
 const express = require('express')
-
 const app = express()
+const db = require('./db')
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
-app.set("View Engine", "hbs")
+app.set("view engine", "hbs")
+
+app.get('/', (req,res) => {
+    
+   db.getAllPersons()
+     .then((persons) => {
+        res.render('persons', {persons})
+     })
+     .catch((err) => {
+        res.send(err)
+     })
+
+    
+})
+
+app.get('/add', (req,res) => {
+    res.render('person_add')
+})
+
+app.post('/add',(req,res) => {
+   db.addNewPerson(req.body.name, req.body.age, req.body.city)
+   .then(() => {
+       res.redirect('/')
+   })
+   .catch((err) => {
+       res.send(err)
+   })
+})
 
 app.listen(8888, ()=> {
     console.log('Server Started On http://localhost:8888/');
