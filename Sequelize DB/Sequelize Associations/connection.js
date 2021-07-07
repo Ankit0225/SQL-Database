42b84c0f2d05ec8fcb0707373ba20154b86504fb
Ankit <ankitmaurya0225@gmail.com>
2021-07-07 20:23:43 +0530

    Sequelize Associations

---
+++ Sequelize DB/Sequelize Associations/connection.js
@@ -0,0 +1,15 @@
+const Sequelize = require('sequelize')
+
+const db = new Sequelize('sampledb', 'sampleuser', 'samplepass', {
+    host: 'localhost',
+    dialect: 'mysql'
+
+})
+db
+ .authenticate()
+ .then(() => {
+     console.log('Connection has been established');
+ })
+ .catch(err => {
+    console.error('Unable to Connect Database', err);
+ });
