75405ac817a5a1c54e70fa3e02f0caafc22c63bf
Ankit <ankitmaurya0225@gmail.com>
2021-07-08 23:01:58 +0530

    Sequelize Query

--- Sequelize DB/Sequelize Query/query.js
+++ Sequelize DB/Sequelize Query/query.js
@@ -5,9 +5,16 @@ const task = async () => {
 
     await db.sync()
 
-    const students = await Student.findAll(`
-      where age:  
-    `);
+    const students = await Student.findAll({
+    //  what you what to find write here
+        /* Example: 
+      where: {age: 13},
+       order: [
+          ['name', 'ASC'],
+          ['age', 'DESC']
+       ] */
+      //  By writing these you will get all related datas 
+    });
     students.forEach(student => console.log(`
     name: ${student.dataValues.name} \t age: ${student.dataValues.age} \t 
     city: ${student.dataValues.city} `))
