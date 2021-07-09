b67b910f9b4abbb1f7d472c7187eaaa54899b9a4
Ankit <ankitmaurya0225@gmail.com>
2021-07-09 13:14:42 +0530

    testing phase

---
+++ Sequelize DB/Sequelize Seeding Database with Initial Values/seeding.js
@@ -0,0 +1,28 @@
+const {db, Center, Season, Batch, Course, Teacher} = require('./model')
+
+const seed = async() => {
+    try {
+      
+
+        db.sync({alter: true})
+
+            await Center.buildCreate([
+            {id: 'DN', name: 'Baburao' , city: 'Delhi'},
+            {id: 'DN', name: 'Priya' , city: 'Delhi'},
+            {id: 'LON', name: 'Chaitanya' , city: 'London'},
+            {id: 'TOK', name: 'Gauti' , city: 'Tokyo'},
+            {id: 'MEL', name: 'Blazr' , city: 'Delhi'},
+            {id: 'SYN', name: 'Online' , city: 'Syndey'}
+        ],{
+
+            ignoreDuplicates: true
+        }
+        )
+    }
+    catch (err){
+
+        console.log(err);
+    }
+}
+
+seed()
