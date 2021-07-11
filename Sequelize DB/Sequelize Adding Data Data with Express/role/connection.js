d05d8b0f48a8ddbebfb29aa272551adffba03496
Ankit <ankitmaurya0225@gmail.com>
2021-07-12 00:28:14 +0530

    Sequelize Adding Data Data with Express

---
+++ Sequelize DB/Sequelize Adding Data Data with Express/role/connection.js
@@ -0,0 +1,17 @@
+const Sequelize = require('sequelize')
+
+const db = new Sequelize('sampledb2', 'sampleuser1', 'samplepass1', {
+    host: 'localhost',
+    dialect: 'mysql'
+
+})
+db
+ .authenticate()
+ .then(()  => {
+     console.log('Connection has been established');
+ })
+ .catch(err => {
+    console.error('Unable to Connect Database', err);
+ });
+
+ module.exports = {db}
---
+++ Sequelize DB/Sequelize Adding Data Data with Express/server.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const path = require('path')
+const {Center,Course,Season} = require('./role/model')
+
+const app = express()
+
+app.set('view engine', 'hbs')
+app.set('views', path.resolve(__dirname, './views'))
+
+app.get('/index', async (req, res) => {
+
+  try {
+      const centers = await Center.findAll()
+      const courses = await Course.findAll()
+      const seasons = await Season.findAll()
+      const years = [2021,2022,2023,2024,2025]
+    res.render('index', {
+        centers, courses, seasons, years
+    })
+
+  } catch (err) {
+      console.error(err);
+  }
+    
+})
+
+module.exports = {
+    app
+}
